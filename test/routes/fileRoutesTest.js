da4d84fb80277550e9eee650392ed53e0e04a002
AlenaBukilic <bukilicalena@gmail.com>
2018-03-27 21:51:01 +0200

    fileRoutesTest - download file

--- test/routes/fileRoutesTest.js
+++ test/routes/fileRoutesTest.js
@@ -14,7 +14,7 @@ describe('File requests', function(){
 
     const request = chai.request;
 
-    describe('Create file', function(){        
+    describe('Upload file', function(){        
 
         describe('Valid params', function() {
 
@@ -37,6 +37,36 @@ describe('File requests', function(){
                 request('http://localhost:8000')
                 .post('/issues/5aba5d0301f3703b5c72045/comments')
                 .attach('file', fs.readFileSync('./test/facade/fakeFile.txt'), 'fakeFile.txt')
+                .then(done, (err) => {
+                    expect(err).to.not.be.null;
+                    expect(err).to.have.status(500);
+                    done();
+                })
+                .catch(done);
+            });
+        });
+    });
+    describe('Download file', function(){        
+
+        describe('Valid params', function() {
+
+            it('should download file', function(done){
+                request('http://localhost:8000')
+                .get('/files/5aba9a16c63a752d8c0f6c2f')
+                .then((response) => {
+                    expect(response).to.not.be.null;
+                    expect(response).to.have.status(200);
+                    expect(response.text).to.include('blah')
+                    done();
+                }, done)
+                .catch(done);
+            });
+        });
+        describe('Invaild params', function() {
+    
+            it('should not download file', function(done){
+                request('http://localhost:8000')
+                .get('/files/5aba9a16c63a72d8c0f6c2f')
                 .then(done, (err) => {
                     expect(err).to.not.be.null;
                     expect(err).to.have.status(500);
